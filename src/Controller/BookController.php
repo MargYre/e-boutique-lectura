8c20212163dcc05547338dc2c147c6c8b7fa10d7
Margaux <margaux.tarayre@gmail.com>
2025-04-11 20:25:09 +0200

    feat: implement category filtering for books

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,8 +2,9 @@
 namespace App\Controller;
 
 use App\Repository\BookRepository;
-use App\Repository\CategoryRepository; // <-- N'oubliez pas ce use !
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,11 +13,19 @@ class BookController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function home(
         BookRepository $bookRepo,
-        CategoryRepository $categoryRepo // <-- Injection correcte
+        CategoryRepository $categoryRepo,
+        Request $request // <-- Ajoutez ceci
     ): Response {
-        return $this->render('home/index.html.twig', [
-            'books' => $bookRepo->findAll(),
-            'categories' => $categoryRepo->findAll(),
-        ]);
+    $categorySlug = $request->query->get('category');
+    
+    $books = $categorySlug 
+        ? $bookRepo->findBy(['category' => $categoryRepo->findOneBy(['slug' => $categorySlug])])
+        : $bookRepo->findAll();
+
+    return $this->render('home/index.html.twig', [
+        'books' => $books,
+        'categories' => $categoryRepo->findAll(),
+        'currentCategory' => $categorySlug // Pour le highlight actif
+    ]);
     }
 }
