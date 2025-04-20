f48477079fa7fbc33b3d754d6a0865167eb5d515
Margaux <margaux.tarayre@gmail.com>
2025-04-21 01:11:26 +0200

    Auth: add: Lorsque l'utilisateur se connecte à la boutique, ses paramètres de connexion le suivront en variable de session.~

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,39 +4,58 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Security\LoginFormAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
-    {
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface $entityManager,
+        UserAuthenticatorInterface $userAuthenticator,
+        LoginFormAuthenticator $authenticator
+    ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var string $plainPassword */
             $plainPassword = $form->get('plainPassword')->getData();
-
-            // encode the plain password
             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // do anything else you need here, like send an email
+            // Solution 1: Désactiver Turbo pour cette réponse
+            $request->attributes->set('_turbo', false);
 
-            return $this->redirectToRoute('homepage');
+            // Authentification et redirection
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            ) ?: $this->redirectToRoute('homepage');
+        }
+
+        // Solution alternative pour le rendu du formulaire
+        if ($request->headers->get('Turbo-Frame')) {
+            $response = $this->render('registration/register.html.twig', [
+                'registrationForm' => $form->createView(),
+            ]);
+            $response->headers->set('Turbo-Frame', 'registration_form');
+            return $response;
         }
 
         return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form,
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
