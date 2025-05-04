34bdad0bd15f0c2bf33e84afc811d84f7231c102
Margaux <margaux.tarayre@gmail.com>
2025-05-04 22:04:39 +0200

    Updatedisplay of admin page

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UserRepository;
 
 final class AdminUserController extends AbstractController
 {
     #[Route('/admin/user', name: 'app_admin_user')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('admin_user/index.html.twig', [
-            'controller_name' => 'AdminUserController',
+            'users' => $userRepository->findAll(),
         ]);
     }
 }
